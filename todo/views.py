58c3b3aad6fbaa4242fdc190f803981cfcaa79b5
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-17 15:58:29 -0500

    creates login error handling and redirect upon successful login

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def home(request):
@@ -35,18 +35,14 @@ def loginuser(request):
     if request.method == 'GET':
       return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
-      if request.POST['password1'] == request.POST['password2']:
-        try:
-          user = User.objects.create_user(request.POST['username'], request.POST['password1'])
-          user.save()
-          login(request, user)
-          return redirect('currenttodos')
-        except IntegrityError:
-          return render(request, 'todo/registeruser.html', {'form':UserCreationForm(), 'error':'Username is already taken. Please choose a new one!'})
+      user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+
+      if user is None:
+         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match!'})
       else:
-        return render(request, 'todo/registeruser.html', {'form':UserCreationForm(), 'error':'Passwords did not match!'})
-  
-   
+        login(request, user)
+        return redirect('currenttodos')
+        
 def logoutuser(request):
   if request.method == 'POST':
     logout(request)
