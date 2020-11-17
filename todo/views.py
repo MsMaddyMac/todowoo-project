07fe75aa8d20ef2b9156c66d4616340e56ec5c29
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-17 15:04:51 -0500

    adds error handling to enable unique username

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def registeruser(request):
@@ -13,9 +14,12 @@ def registeruser(request):
     # invoke the User() with built in methods and pass in the request info from the form by referencing the keys of each input to be used.
     # If password1 is equal to password2 create new user
     if request.POST['password1'] == request.POST['password2']:
-        # Create a new user
-      user = User.objects.create_user(request.POST['username'], request.POST['password1'])
-        #  Save new user in database
-      user.save()
+      try:
+          # Create a new user
+        user = User.objects.create_user(request.POST['username'], request.POST['password1'])
+          #  Save new user in database
+        user.save()
+      except IntegrityError:
+        return render(request, 'todo/registeruser.html', {'form':UserCreationForm(), 'error':'Username is already taken. Please choose a new one!'})
     else:
-      print('hello')
+      return render(request, 'todo/registeruser.html', {'form':UserCreationForm(), 'error':'Passwords did not match!'})
