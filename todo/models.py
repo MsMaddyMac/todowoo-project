711b043a1043e5042eebe90d9844150828829b31
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-17 18:45:00 -0500

    updates admin.py to allow created_on to display as readonly field

--- todo/models.py
+++ todo/models.py
@@ -6,9 +6,10 @@ class Todo(models.Model):
   title = models.CharField(max_length=100)
   memo = models.TextField(blank=True)
   created_on = models.DateTimeField(auto_now_add=True)
-  date_completed = models.DateTimeField(null=True)
+  date_completed = models.DateTimeField(null=True, blank=True)
   important = models.BooleanField(default=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   
   
-  
+  def __str__(self):
+    return self.title
