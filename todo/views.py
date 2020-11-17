21961352b2a92aaed97f565335f025626957569a
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-17 14:30:54 -0500

    import django usercreationform and renders it in registeruser.html

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def registeruser(request):
-  return render(request, 'todo/registeruser.html')
+  # to use Django generated form, create import first
+  # render form in registeruser.html
+  return render(request, 'todo/registeruser.html', {'form':UserCreationForm()})
